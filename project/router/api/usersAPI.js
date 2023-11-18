e8fd6924e45cf8b75eac4671444fd6a4052ee3b0
RickSanchezzz666 <lapiyktube@gmail.com>
2023-11-18 16:36:58 +0200

    Error Handler Created!

--- project/router/api/usersAPI.js
+++ project/router/api/usersAPI.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { UsersController } = require('../../controllers');
+const { apiWrapper } = require('../apiWrapper/index')
 // const passport = require('passport');
 const router = Router();
 
-router.post("/login", UsersController.login);
+router.post("/login", apiWrapper(UsersController.login));
 
 module.exports = { router };
