e8d1df30160bb4c84304503e29c04786b31819b4
RickSanchezzz666 <lapiyktube@gmail.com>
2023-12-01 19:40:28 +0200

    Post Creation Photos Fix!

--- project/models/contentModel.js
+++ project/models/contentModel.js
@@ -5,18 +5,18 @@ const schema = new Schema({
     p_Title: { type: String, required: true },
     p_TextContent: { type: String, required: true },
     p_ShortDescription: { type: String, required: true },
-    p_Photos: {
-        p_Photo1: { type: Buffer, required: true },
-        p_Photo2: { type: Buffer },
-        p_Photo3: { type: Buffer },
-        p_Photo4: { type: Buffer },
-        p_Photo5: { type: Buffer },
-        p_Photo6: { type: Buffer },
-        p_Photo7: { type: Buffer },
-        p_Photo8: { type: Buffer },
-        p_Photo9: { type: Buffer },
-        p_Photo10: { type: Buffer }
-    },
+    p_Photos: [ 
+        {p_Photo1: { type: Buffer, required: true }},
+        {p_Photo2: { type: Buffer, default: null }},
+        {p_Photo3: { type: Buffer, default: null }},
+        {p_Photo4: { type: Buffer, default: null }},
+        {p_Photo5: { type: Buffer, default: null }},
+        {p_Photo6: { type: Buffer, default: null }},
+        {p_Photo7: { type: Buffer, default: null }},
+        {p_Photo8: { type: Buffer, default: null }},
+        {p_Photo9: { type: Buffer, default: null }},
+        {p_Photo10: { type: Buffer, default: null }}
+    ],
     p_Timestamp: { type: Date, required: true }
 });
 
