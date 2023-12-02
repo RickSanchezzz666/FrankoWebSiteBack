780d14a9392a36ff982099553402983504e5d497
RickSanchezzz666 <lapiyktube@gmail.com>
2023-12-02 21:47:50 +0200

    Merge fix

--- project/controllers/postsController/createPost.js
+++ project/controllers/postsController/createPost.js
@@ -2,30 +2,34 @@ const axios = require('axios');
 const { ContentModel } = require('../../models/contentModel')
 
 module.exports.createPost = async (req, res) => {
-    try {
-        let idCount = await ContentModel.countDocuments();
-        const { p_Title, p_TextContent, p_ShortDescription, p_Photos } = req.body
-        
-        p_Timestamp = new Date().toLocaleString()
+    if (req.user.u_AccessLevel === 0 | req.user.u_AccessLevel === 1)
+        try {
+            let idCount = await ContentModel.countDocuments();
+            const { p_Title, p_TextContent, p_ShortDescription, p_Photos } = req.body
 
-        let photoBufferArray = [];
+            p_Timestamp = new Date().toLocaleString()
 
-        if (Array.isArray(p_Photos)) {
-            for (let i = 1; i <= 10; i++) {
-                const photoKey = `p_Photo${i}`;
-                const photoObject = p_Photos.find(photoObj => photoObj.hasOwnProperty(photoKey) && photoObj[photoKey] !== null);
-                if (photoObject) {
-                    const response = await axios.get(photoObject[photoKey], { responseType: 'arraybuffer' });
-                    const photoBuffer = Buffer.from(response.data);
-                    photoBufferArray[photoKey] = photoBuffer;
+            let photoBufferArray = [];
+
+            if (Array.isArray(p_Photos)) {
+                for (let i = 1; i <= 10; i++) {
+                    const photoKey = `p_Photo${i}`;
+                    const photoObject = p_Photos.find(photoObj => photoObj.hasOwnProperty(photoKey) && photoObj[photoKey] !== null);
+                    if (photoObject) {
+                        const response = await axios.get(photoObject[photoKey], { responseType: 'arraybuffer' });
+                        const photoBuffer = Buffer.from(response.data);
+                        photoBufferArray[photoKey] = photoBuffer;
+                    }
                 }
             }
-        }
 
-        const newPost = new ContentModel({p_Id: idCount + 1, p_Title, p_TextContent, p_ShortDescription, p_Photos: photoBufferArray, p_Timestamp})
-        await newPost.save()
-        res.status(200).send({message: "Post succesfully created"})
-    } catch (error) {
-        res.status(500).send({ message: "Internal server error: ", error })
-    }
+            const newPost = new ContentModel({ p_Id: idCount + 1, p_Title, p_TextContent, p_ShortDescription, p_Photos: photoBufferArray, p_Timestamp })
+            await newPost.save()
+            res.status(200).send({ message: "Post succesfully created" })
+        } catch (error) {
+            res.status(500).send({ message: "Internal server error: ", error })
+        }
+    else {
+        return res.status(403).send({ message: "Your access level is not enough." })
+    };
 }
