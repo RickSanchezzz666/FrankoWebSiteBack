09db32b2c86f500bc9c3e12841fb89b8eb352a92
Maksym67 <maxpasternak54@gmail.com>
2023-12-02 21:28:45 +0200

    Auth middleware added / endpoints minor fixes

--- project/controllers/postsController/createPost.js
+++ project/controllers/postsController/createPost.js
@@ -1,15 +1,19 @@
 const { ContentModel } = require('../../models/contentModel')
 
 module.exports.createPost = async (req, res) => {
-    try {
-        let idCount = await ContentModel.countDocuments();
-        const { p_Title, p_TextContent, p_ShortDescription, p_Photos } = req.body
-        
-        p_Timestamp = new Date().toLocaleString()
+    if (req.user.u_AccessLevel === 0 | req.user.u_AccessLevel === 1)
+        try {
+            let idCount = await ContentModel.countDocuments();
+            const { p_Title, p_TextContent, p_ShortDescription, p_Photos } = req.body
+            
+            p_Timestamp = new Date().toLocaleString()
 
-        const newPost = new ContentModel({p_Id: idCount + 1, p_Title, p_TextContent, p_ShortDescription, p_Photos, p_Timestamp})
-        await newPost.save()
-    } catch (error) {
-        res.status(500).send({ message: "Internal server error: ", error })
-    }
+            const newPost = new ContentModel({p_Id: idCount + 1, p_Title, p_TextContent, p_ShortDescription, p_Photos, p_Timestamp})
+            await newPost.save()
+        } catch (error) {
+            res.status(500).send({ message: "Internal server error: ", error })
+        }
+    else {
+        return res.status(403).send({ message: "Your access level is not enough." })
+    };
 }
--- project/controllers/usersController/createAdmin.js
+++ project/controllers/usersController/createAdmin.js
@@ -2,16 +2,32 @@ const { UsersModel } = require('../../models/usersModel')
 const bcrypt = require('bcrypt')
 
 module.exports.createAdmin = async (req, res) => {
-    try {
-        let idCount = await UsersModel.countDocuments();
-        const { login, password, fullname } = req.body;
-        const salt = await bcrypt.genSalt(10)
-        const encryptedPassword = await bcrypt.hash(password, salt)
-        
-        const newAdmin = new UsersModel({ u_Id: idCount + 1, u_Login: login, u_Password: encryptedPassword, u_Fullname: fullname, u_AccessLevel: 1 });
-        await newAdmin.save();
-        return res.status(200).send({message: "You successfully created an Admin!"});
-    } catch (error) {
-        res.status(500).send({ message: "Internal server error: ", error })
+    if (req.user.u_AccessLevel === 1)
+        try {
+            let idCount = await UsersModel.countDocuments();
+            const { login, password, fullname } = req.body;
+            if (!login) {
+                return res.status(400).send({ message: "Parameter 'login' is required." });
+            } 
+            if (!password) {
+                return res.status(400).send({ message: "Parameter 'password' is required." });
+            }
+            if (!fullname) {
+                return res.status(400).send({ message: "Parameter 'Fullname' is required." });
+            }
+            const existingUser = await UsersModel.findOne({ u_Login: login });
+            if (existingUser) {
+                return res.status(409).send({ message: "User with this login already exists." });
+            }
+            const salt = await bcrypt.genSalt(10)
+            const encryptedPassword = await bcrypt.hash(password, salt)
+            const newAdmin = new UsersModel({ u_Id: idCount + 1, u_Login: login, u_Password: encryptedPassword, u_Fullname: fullname, u_AccessLevel: 1 });
+            await newAdmin.save();
+            return res.status(200).send({message: "You successfully created an Admin!"});
+        } catch (error) {
+            res.status(500).send({ message: "Internal server error: ", error })
+        }
+    else {
+        return res.status(403).send({ message: "Your access level is not enough." })
     }
 }
