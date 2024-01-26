85d490b59fdcbf778f587802bdd9750d5dfbd0c6
kristi5491 <ksavchin05@gmail.com>
2024-01-26 16:40:35 +0200

    test getLogs

---
+++ project/test_controllers/test_logsController/getLogs.spec.js
@@ -0,0 +1,21 @@
+const { getLogs } = require('../../controllers/logsController/getLogs')
+
+describe('getLogs', () => {
+    const res = {
+        send: jest.fn(),
+        status: jest.fn().mockReturnThis()
+    }
+
+    const err = new Error;
+    const reqErr = jest.fn().mockImplementation(async () => {throw err})
+
+    it('should be opened and throw error 500 and send message', async () =>{
+        await getLogs(reqErr, res);
+
+        expect(res.status).toHaveBeenCalledWith(500)
+        expect(res.send).toHaveBeenCalledWith({ message: "Внутрішня помилка сервера, зверніться до технічного адміністратора"})
+    })
+
+
+
+})
---
+++ project/test_controllers/test_logsController/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+ // const req = {
+    //     user:{
+    //         accessLevel: 2
+    //     }
+
+    // }
